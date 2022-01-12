495b94788b3a005aceb5469c5a48eab3e7ba0ec7
Rodrigo Oliveira <rodrigoyukimide@gmail.com>
2022-01-12 11:50:11 -0300

    Update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,5 +2,6 @@ function gerarMatricula(){
 	var txt = "ACA";
 //	Gerando um numero qualquer JS
 	var aleatorio = Math.floor(Math.random() * 1500);
-	document.getElementById('matricula').valeu = (txt + aleatorio);
+	document.getElementById('matricula').value = (txt + aleatorio);
 }
+
